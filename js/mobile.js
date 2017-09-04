835b4fc7caa584c7bb880e3ca5df8dcecaa91e8a
Eric Do <docomo102030@gmail.com>
2017-09-05 01:22:54 +0700

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,15 +1,15 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
   switch (document.title) {
-    case "/home/miracle3212":
+    case "/home/zard777":
       window.location.href = '/mobile/index-mob.html';
       break;
-    case "/home/miracle3212/bio":
+    case "/home/zard777/bio":
       window.location.href = '/mobile/bio-mob.html';
       break;
-    case "/home/miracle3212/contact":
+    case "/home/zard777/contact":
       window.location.href = '/mobile/contact-mob.html';
       break;
-    case "/home/miracle3212/projects":
+    case "/home/zard777/projects":
       window.location.href = '/mobile/projects-mob.html';
       break;
     default:
