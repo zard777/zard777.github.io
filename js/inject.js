71721982f04d933e33bf16eec54cdeb5bc8b59ae
Eric D <docomo102030@gmail.com>
2018-03-18 04:17:15 +0700

    \\

--- js/inject.js
+++ js/inject.js
@@ -30,8 +30,7 @@ switch (document.title) {
     document.getElementById('cdp1').href = "javascript:window.location.reload();";
     document.getElementById('cdp2').href = "/index.html";
     document.getElementById('pa').href = "https://github.com/zard777/xbot_remastered";
-    document.getElementById('pm').href = "https://github.com/zard777/TrustTrees";
-    document.getElementById('ps').href = "https://github.com/zard777/wifi-monitor";
+    document.getElementById('pm').href = "https://github.com/mandatoryprogrammer/TrustTrees/pull/7";
     document.getElementById('tf').href = "https://0x12345.blogspot.com/2017/07/files-transfer-w-wormhole-cross-platform.html";
     break;
   default:
